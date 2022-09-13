bd4a2de9133ecf00b4e9a70dab11e6675598634c
Ákos Balogh <akosbalogh16@gmail.com>
2022-09-13 12:26:23 +0200

    Added comment, Seperated function

--- week_1/hello_world.java
+++ week_1/hello_world.java
@@ -1,7 +1,14 @@
 package week_1;
 
 class HelloWorld {
+    
     public static void main(String[] args) {
+        PrintHelloWorld();
+    }
+    
+    // Print out "Hello World!"
+    private static void PrintHelloWorld()
+    {
         System.out.println("Hello World!");
     }
 }
