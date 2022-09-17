556d8fc3f13e4d623c5276329c8699711ec9e21d
Ákos Balogh <akosbalogh16@gmail.com>
2022-09-18 00:10:20 +0200

    Week 2: Bugfix #1

--- NHF2/hello-world/src/main/java/hu/akkkkos/Main.java
+++ NHF2/hello-world/src/main/java/hu/akkkkos/Main.java
@@ -7,13 +7,18 @@ public class Main {
         if (args.length == 0) System.out.println("Nincs elég argumentum!");
         else {
             String lang = "N/A";
-            if (args.length != 1){
+            if (args.length == 1) lang = "hun";
+            else {
                 if (args[1].equals("eng")) lang = "eng";
                 else if (args[1].equals("hun")) lang = "hun";
                 else if (args[1].equals("haw")) lang = "haw";
             }
-            String result = Hello(args[0], lang);
-            System.out.println(result);
+
+            if (lang.equals("N/A")) System.out.println("Ismeretlen nyelv! (hun, eng, haw)");
+            else {
+                String result = Hello(args[0], lang);
+                System.out.println(result);
+            }
         }
     }
 }
