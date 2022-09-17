14ff83e6ecba0cf5a053169187b07be69e9aa34f
Ákos Balogh <akosbalogh16@gmail.com>
2022-09-17 23:53:14 +0200

    Week 2: Done

--- NHF2/hello-world/src/main/java/hu/akkkkos/Main.java
+++ NHF2/hello-world/src/main/java/hu/akkkkos/Main.java
@@ -1,7 +1,19 @@
 package hu.akkkkos;
 
+import static hu.akkkkos.HelloLibrary.Hello;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length == 0) System.out.println("Nincs elég argumentum!");
+        else {
+            String lang = "N/A";
+            if (args.length != 1){
+                if (args[1].equals("eng")) lang = "eng";
+                else if (args[1].equals("hun")) lang = "hun";
+                else if (args[1].equals("haw")) lang = "haw";
+            }
+            String result = Hello(args[0], lang);
+            System.out.println(result);
+        }
     }
 }
